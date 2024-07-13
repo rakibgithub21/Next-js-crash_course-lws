b2493e7d5c9a192495179053f27435bfbfa1d0bd
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-07-13 08:51:53 +0600

    font optimization

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -9,7 +9,9 @@ const page = () => {
         <div>
             <h3>This is mission page</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic molestiae facere mollitia minima reprehenderit quasi aperiam veritatis aut qui fugiat doloremque blanditiis iure laboriosam quod sunt non repudiandae asperiores nostrum nesciunt rem culpa dolor, corrupti autem. Laborum voluptate error nemo cumque alias qui. Distinctio ab dolores repellat doloribus quia totam.</p>
-            <Image src={thumImage} alt="Thumbnail next.js course"/>
+            <div className="w-[400px] border border-red-800">
+                <Image src={thumImage} alt="Thumbnail next.js course" />
+           </div>
             <Button/>
         </div>
     );
